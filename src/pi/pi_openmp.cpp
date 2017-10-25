78fbf0cfd9f3c75e102abc6bdb30a1e449c71610
Sathyam Vellal <sathyam.vellal@gmail.com>
2017-10-25 11:34:03 -0700

    Cleaning up debug code

--- src/pi/pi_openmp.cpp
+++ src/pi/pi_openmp.cpp
@@ -2,7 +2,6 @@
 // Created by Sathyam Vellal on 24/10/2017.
 //
 
-#include <iostream>
 #include "omp.h"
 
 #include "pi/pi.h"
@@ -15,11 +14,8 @@ double pi() {
     double x;
     double sum = 0.0;
 
-    omp_set_num_threads(8);
-
     #pragma omp parallel private(x) reduction(+:sum)
     {
-        std::cout << "Boo" << std::endl;
         #pragma omp for
         for (long int i = 0; i < N; ++i) {
             x = (i + 0.5) * step;
