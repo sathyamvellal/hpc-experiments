0bf8ab18e5795a11184984e649d9381753e1ac08
Sathyam Vellal <sathyam.vellal@gmail.com>
2017-10-25 12:56:41 -0700

    Adding Wall_Clock time calculation

--- test/pi/test_pi_openmp.cpp
+++ test/pi/test_pi_openmp.cpp
@@ -4,14 +4,20 @@
 
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 #include "omp.h"
 
 #include "pi/pi.h"
 
 int main(int argc, char **argv) {
-    double PI = pi();
+    double PI;
+
+    clock_t time_begin = clock();
+    PI = pi();
+    clock_t time_end = clock();
 
     std::cout << "PI: " << std::setprecision(15) << PI << std::endl;
+    std::cout << "CPU: " << std::setprecision(10) << (double)(time_end - time_begin)/(1000000.0) << std::endl;
 
     return 0;
 }
