2d7ed1de9e5188826fa8a34ec75b2d3e9d698249
Sathyam Vellal <sathyam.vellal@gmail.com>
2017-10-25 07:00:46 -0700

    Sequential PI

---
+++ include/pi/pi_seq.h
@@ -0,0 +1,10 @@
+//
+// Created by Sathyam Vellal on 24/10/2017.
+//
+
+#ifndef HPC_EXPERIMENTS_PI_H
+#define HPC_EXPERIMENTS_PI_H
+
+double pi();
+
+#endif //HPC_EXPERIMENTS_PI_H
---
+++ src/pi/pi_seq.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Sathyam Vellal on 24/10/2017.
+//
+
+#include "pi/pi_seq.h"
+
+
+double pi() {
+    const long int N = 1000l * 1000l * 1000l * 10l;
+    const double step = 1.0 / N;
+
+    double x;
+    double sum = 0.0;
+
+    for (long int i = 0; i < N; ++i) {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    return sum / N;
+}
